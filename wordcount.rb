d99da0a0325433381abba776640d0823559c3bc2
Steve Purcell <steve.purcell@powershop.co.nz>
2015-06-29 14:22:02 +1200

    More granular methods

--- wordcount.rb
+++ wordcount.rb
@@ -13,14 +13,13 @@ class WordCount
     occurrences = 0
     while ensure_buffer
       ## Just optimistically look for the string in the buffer
-      if found_pos = @buffer.index(@string)
-        end_offset = found_pos + @string.size
-        @buffer = @buffer.slice(end_offset..-1)
+      if found_pos = index(@string)
+        skip_to(found_pos + @string.size)
         occurrences += 1
       else
         # Throw away the part of the buffer we know can't contain part of the string
         first_char_pos = first_possible_starting_pos
-        @buffer = first_char_pos ? @buffer.slice(first_char_pos..-1) : ""
+        @buffer = first_char_pos ? skip_to(first_char_pos) : ""
       end
     end
     occurrences
@@ -28,11 +27,19 @@ class WordCount
 
   private
 
+  def skip_to(pos)
+    @buffer = @buffer.slice(pos..-1)
+  end
+
+  def index(str, offset=0)
+    @buffer.index(str, offset)
+  end
+
   def first_possible_starting_pos
     ## Searching for foo
     ##  0 1 2 3 4 5 6 7 8 9 | STREAM
     ##  x x x x x x x x f o | o
-    @buffer.index(@string[0], @buffer.size - @string.size + 1)
+    index(@string[0], @buffer.size - @string.size + 1)
   end
 
   def ensure_buffer
