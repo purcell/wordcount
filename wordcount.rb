b0de3c91dfe35e404c8b3029ea73ba1c2464915f
Steve Purcell <steve.purcell@powershop.co.nz>
2015-06-29 15:13:40 +1200

    Extract a separate Buffer class

--- wordcount.rb
+++ wordcount.rb
@@ -1,25 +1,66 @@
 #!/usr/bin/env ruby -w
 
-class WordCount
-  def initialize(stream, string, buffer_size_limit=(1024 ** 2))
-    @stream = stream
-    raise ArgumentError, "empty search string" if string.empty?
-    @string = string
+class Buffer
+  def initialize(buffer_size_limit, &read_chunk)
+    @read_chunk = read_chunk
     @buffer_size_limit = buffer_size_limit
     @buffer = ""
   end
 
+  def empty?
+    @buffer.empty?
+  end
+
+  def index(str, offset=0)
+    @buffer.index(str, offset)
+  end
+
+  def skip_to(pos)
+    @buffer = @buffer.slice(pos..-1)
+  end
+
+  def skip_to_end
+    @buffer = ""
+  end
+
+  def ensure_length(len)
+    if @buffer.empty? || (@buffer.size < len)
+      wanted = @buffer_size_limit - @buffer.size
+      next_chunk = @read_chunk.call(wanted)
+      return false if (next_chunk.nil? || next_chunk.empty?)
+      @buffer += next_chunk
+    else
+      true
+    end
+  end
+
+  def size
+    @buffer.size
+  end
+end
+
+class WordCount
+  def initialize(stream, string, buffer_size_limit=(1024 ** 2))
+    raise ArgumentError, "empty search string" if string.empty?
+    @string = string
+    @buffer = Buffer.new(buffer_size_limit, &stream.method(:read))
+  end
+
   def count_occurrences
     occurrences = 0
-    while ensure_buffer
+    while @buffer.ensure_length(@string.size)
       ## Just optimistically look for the string in the buffer
-      if found_pos = index(@string)
-        skip_to(found_pos + @string.size)
+      if found_pos = @buffer.index(@string)
+        @buffer.skip_to(found_pos + @string.size)
         occurrences += 1
       else
         # Throw away the part of the buffer we know can't contain part of the string
         first_char_pos = first_possible_starting_pos
-        @buffer = first_char_pos ? skip_to(first_char_pos) : ""
+        if first_char_pos
+          @buffer.skip_to(first_char_pos)
+        else
+          @buffer.skip_to_end
+        end
       end
     end
     occurrences
@@ -27,34 +68,11 @@ class WordCount
 
   private
 
-  def skip_to(pos)
-    @buffer = @buffer.slice(pos..-1)
-  end
-
-  def index(str, offset=0)
-    @buffer.index(str, offset)
-  end
-
   def first_possible_starting_pos
     ## Searching for foo
     ##  0 1 2 3 4 5 6 7 8 9 | STREAM
     ##  x x x x x x x x f o | o
-    index(@string[0], @buffer.size - @string.size + 1)
-  end
-
-  def ensure_buffer
-    buffer_smaller_than_string? ? read_more_into_buffer : true
-  end
-
-  def buffer_smaller_than_string?
-    @buffer.empty? || (@buffer.size < @string.size)
-  end
-
-  def read_more_into_buffer
-    wanted = @buffer_size_limit - @buffer.size
-    next_chunk = @stream.read(wanted)
-    return false if (next_chunk.nil? || next_chunk.empty?)
-    @buffer += next_chunk
+    @buffer.index(@string[0], @buffer.size - @string.size + 1)
   end
 end
 
