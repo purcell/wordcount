c5ad65f5f3f186721b6b37d9fbcf5ee00d8e1b86
Steve Purcell <steve.purcell@powershop.co.nz>
2015-06-29 13:48:29 +1200

    Refactor to inject all values via constructor

--- wordcount.rb
+++ wordcount.rb
@@ -1,23 +1,25 @@
 #!/usr/bin/env ruby -w
 
 class WordCount
-  def initialize(stream, buffer_size_limit=(1024 ** 2))
+  def initialize(stream, string, buffer_size_limit=(1024 ** 2))
     @stream = stream
+    raise ArgumentError, "empty search string" if string.empty?
+    @string = string
     @buffer_size_limit = buffer_size_limit
     @buffer = ""
   end
 
-  def count_occurrences(string)
+  def count_occurrences
     occurrences = 0
-    while ensure_buffer(string)
+    while ensure_buffer
       ## Just optimistically look for the string in the buffer
-      if found_pos = @buffer.index(string)
-        end_offset = found_pos + string.size
+      if found_pos = @buffer.index(@string)
+        end_offset = found_pos + @string.size
         @buffer = @buffer.slice(end_offset..-1)
         occurrences += 1
       else
         # Throw away the part of the buffer we know can't contain part of the string
-        first_char_pos = first_possible_starting_pos(string)
+        first_char_pos = first_possible_starting_pos
         @buffer = first_char_pos ? @buffer.slice(first_char_pos..-1) : ""
       end
     end
@@ -26,19 +28,19 @@ class WordCount
 
   private
 
-  def first_possible_starting_pos(string)
+  def first_possible_starting_pos
     ## Searching for foo
     ##  0 1 2 3 4 5 6 7 8 9 | STREAM
     ##  x x x x x x x x f o | o
-    @buffer.index(string[0], @buffer.size - string.size + 1)
+    @buffer.index(@string[0], @buffer.size - @string.size + 1)
   end
 
-  def ensure_buffer(string)
-    buffer_too_small_to_contain?(string) ? read_more_into_buffer : true
+  def ensure_buffer
+    buffer_smaller_than_string? ? read_more_into_buffer : true
   end
 
-  def buffer_too_small_to_contain?(string)
-    @buffer.empty? || (@buffer.size < string.size)
+  def buffer_smaller_than_string?
+    @buffer.empty? || (@buffer.size < @string.size)
   end
 
   def read_more_into_buffer
@@ -49,5 +51,4 @@ class WordCount
   end
 end
 
-wc = WordCount.new(STDIN)
-puts wc.count_occurrences(ARGV.first)
+puts WordCount.new(STDIN, ARGV.first).count_occurrences
